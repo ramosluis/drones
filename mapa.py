dcdd3690f7f0e1b77780726113c67605e326aeee
ramosluis <luisramosmaldonado@gmail.com>
2016-10-11 14:14:12 -0700

    Ya se ve el color de NDVI en el mapa
    Ya se ve cada punto de NDVI en el mapa de un color diferente dependiendo del valor de NDVI.
    
    * Valores negativos son rojos
    * Valores entre 0-0.49 son verde claro
    * Valores mayores o iguales a 0.5 son verde

--- mapa.py
+++ mapa.py
@@ -16,11 +16,8 @@ from bokeh.models import (
   Circle, 
   DataRange1d, 
   PanTool, 
-  WheelZoomTool, 
-  BoxZoomTool,
+  WheelZoomTool,
   HoverTool,
-  ResizeTool,
-  ResetTool,
 )
 
 # para el HoverTool
@@ -33,8 +30,8 @@ from collections import OrderedDict
 ###################################
 
 # seleccionar shapefile que se va a analizar
-shapefile = "C:/Users/Luis/Documents/xantronic/agricultura/vuelos/vuelo_3_25m/vuelo_3_25m/4_index/indices/ndvi/vuelo_3_25m_index_ndvi.shp"
-#shapefile = "C:/Users/Luis/Documents/xantronic/agricultura/vuelos/test/4_index/indices/ndvi/test_index_ndvi.shp"
+#shapefile = "C:/Users/Luis/Documents/xantronic/agricultura/vuelos/vuelo_3_25m/vuelo_3_25m/4_index/indices/ndvi/vuelo_3_25m_index_ndvi.shp"
+shapefile = "C:/Users/Luis/Documents/xantronic/agricultura/vuelos/test/4_index/indices/ndvi/test_index_ndvi.shp"
 # variables para guardar la informacion
 ndvi = []
 coords_utm = []
@@ -48,43 +45,57 @@ dataSource = driver.Open(shapefile, 0)
 layer = dataSource.GetLayer()
 
 for feature in layer:
-    ndvi.append(str(feature.GetField("ndvi")))
+    ndvi.append(float(feature.GetField("ndvi")))
     geom = feature.GetGeometryRef()
     coords_utm.append(geom.Centroid().ExportToWkt())
 
 
-count = 0
+
 # tomar solo los valores de las coordenadas y guardarlas en nuevas variables
-for i in coords_utm:
+for i in range(0, len(coords_utm)):
     # tomamos coordenadas como 4 valores, 1 par numeros enteros y 1 par de decimales
-    coords.append(re.findall('\d+', coords_utm[count]))
+    coords.append(re.findall('\d+', coords_utm[i]))
     # hacemos casting como string para agregar punto decimal
-    coords[count][0] = str(coords[count][0])+"."+str(coords[count][1])
-    coords[count][1] = str(coords[count][2])+"."+str(coords[count][3])
+    coords[i][0] = str(coords[i][0])+"."+str(coords[i][1])
+    coords[i][1] = str(coords[i][2])+"."+str(coords[i][3])
     # poner los valores donde van para facilidad de uso / acceso
-    latitud.append(coords[count][0])
-    longitud.append(coords[count][1])
-    count += 1
+    latitud.append(coords[i][0])
+    longitud.append(coords[i][1])
 
 
 # convertir de UTM a LAT-LON
 # argumentos de la funcion utm.to_latlon(EASTING, NORTHING, ZONE NUMBER, ZONE LETTER)
 # la zona se puede obtener del archivo con extensión PRJ
 # y abriéndolo en un editor de texto
-count = 0
-for i in latitud:
+for i in range(0, len(latitud)):
     # la funcion para convertir solo acepta floats como coordenadas...
-    coords[count] = utm.to_latlon(float(latitud[count]), float(longitud[count]), 12, 'N')
+    coords[i] = utm.to_latlon(float(latitud[i]), float(longitud[i]), 12, 'N')
     # ... pero si se quieren convertir de vuelta a strings usar las siguientes dos lineas
 #    latitud[count] = str(coords[count][0])
 #    longitud[count] = str(coords[count][1])
     # o dejarlos como floats
-    latitud[count] = coords[count][0]
-    longitud[count] = coords[count][1]
-    count += 1
+    latitud[i] = coords[i][0]
+    longitud[i] = coords[i][1]
     
 
+# para generar los colores que se veran en el mapa
+colores_ndvi = []
 
+# de aqui en adelante la lista de NDVI debe ser conformada por strings para ver 
+# los valores bien en el mapa
+for i in range(0, len(ndvi)):
+    # si el NDVI es negativo será rojo
+    if ndvi[i] < 0:
+        colores_ndvi.append("ff0000")
+    # si el NDVI es entre 0 y 0.49 será verde claro
+    elif ndvi[i] > 0 and ndvi[i] < 0.49:
+        colores_ndvi.append("ccff66")
+    # si el NDVI es mayor o igual a 0.5 será verde
+    else:
+        colores_ndvi.append("00cc00")
+    ndvi[i] = str(ndvi[i])
+    
+    
 ###################################
 ###################################
 #### GRAFICAR Y VISUALIZAR MAPA ###
@@ -97,7 +108,7 @@ for i in latitud:
 API_KEY = "AIzaSyAIKNV7XZFPkFZFPGeNnnKHfoYwAegTIB0"
 
 # property type: map_type:Enum(‘satellite’, ‘roadmap’, ‘terrain’, ‘hybrid’)
-map_options = GMapOptions(lat=latitud[0], lng=longitud[0], map_type="terrain", zoom=16)
+map_options = GMapOptions(lat=latitud[0], lng=longitud[0], map_type='hybrid', zoom=16)
 
 # para graficar los valores de NDVI/Coordenadas se usa un tipo de plot especial 
 # que usa Google Maps de fondo
@@ -118,13 +129,13 @@ source = ColumnDataSource(
     data=dict(
         x=longitud,
         y=latitud,
-        indice=ndvi
+        indice=ndvi,
+        colores=colores_ndvi,
     )
 )
 
-# cada valor será representado por un círculo azul
-# TODO: cambiar el color del círculo dependiendo del valor de NDVI
-circle = Circle(x="x", y="y", size=8, fill_color="blue", fill_alpha=0.8, line_color=None)
+# cada valor será representado por un color correspondiente al NDVI
+circle = Circle(x="x", y="y", size=5, fill_color="colores", fill_alpha=0.8, line_color=None)
 
 # generar gráfica
 plot.add_glyph(source, circle)
@@ -138,7 +149,6 @@ hover.tooltips = OrderedDict([
 ])
 
 # guardamos la gráfica como un html para visualizarlo en cualquier navegador
-output_file("gmap_plot.html")
-#output_file("test_plot.html")
+#output_file("gmap_plot.html")
+output_file("test_plot.html")
 show(plot)
-
