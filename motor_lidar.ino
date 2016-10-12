2b3595af8ff76daa8ac9826237da9505e7978056
ramosluis <luisramosmaldonado@gmail.com>
2016-10-12 13:08:22 -0700

    Adición de archivos Arduino
    Programas de Arduino para controlar el motor brushless, LiDar y hacer datalogging

---
+++ motor_lidar.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+ 
+Servo esc;
+int throttlePin = 0;
+ 
+void setup()
+{
+esc.attach(9);
+}
+ 
+void loop()
+{
+int throttle = analogRead(throttlePin);
+// map(variable, valor minimo real, valor maximo real, valor minimo out, valor maximo out)
+throttle = map(throttle, 0, 1023, 1000, 2000);
+esc.writeMicroseconds(throttle);
+}
