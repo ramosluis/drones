1a90eadb63c9db7852345584a3ecdabae9f7ed1d
ramosluis <luisramosmaldonado@gmail.com>
2016-11-10 14:41:26 -0700

    Update lidar_imu_gps.py

--- lidar_imu_gps.py
+++ lidar_imu_gps.py
@@ -1,12 +1,13 @@
 import serial
 import time
 import csv
-#import pigpio
+import pigpio
 from time import sleep
 
+# objeto para manipular GPIO del Pi
+pi = pigpio.pi()
 
-#pi = pigpio.pi()
-
+# Para mover el motor via el ESC
 #pi.set_servo_pulsewidth(17,0)
 #sleep(2)
 
@@ -23,6 +24,7 @@ dataFile = open('/home/pi/Documents/python/datalog/'+timestr+'.csv', 'w')
 dataWriter = csv.writer(dataFile)
 data = []
 
+# objetos para leer sensores
 gps = serial.Serial(port='/dev/gps',
 					baudrate=57600,
 					parity=serial.PARITY_NONE,
@@ -40,16 +42,34 @@ lidar = serial.Serial('/dev/ttyAMA0',
 imu = serial.Serial('/dev/arduinoUNO', 115200)
 
 while 1:
+	# guardamos valores de sensores
 	x = gps.readline()
 	y = imu.readline()
+	# los valores del lidar vienen codificados como caracteres ASCII
+	# con la funcion ord() los cambiamos a decimal
 	data_H = ord(lidar.read(1))
 	data_L = ord(lidar.read(1))
+	# las oraciones que empiezan con $GPRMC y $GPGGA son las que nos dan
+	# informacion util del GPS
 	if "$GPRMC" in x or "$GPGGA" in x:
 		data.append(x)
 		data.append(y)
 		data.append(data_H)
 		data.append(data_L)
+		# encendemos LED indicador de escritura de datos
+		pi.write(17, 1)
 		dataWriter.writerow([data])
+		pi.write(17, 0)
+		# borramos lista para tomar datos nuevos
 		data[:] = []
+		# si se comentan las siguientes tres lineas se pueden escribir 
+		# 2x la cantidad de datos
+		gps.flushInput()
+		imu.flushInput()
+		lidar.flushInput()
 	else:
-			continue
+		# si no entra una de las oraciones del GPS necesarias
+		# limpiamos el bufer serial de los datos
+		gps.flushInput()
+		imu.flushInput()
+		lidar.flushInput()
