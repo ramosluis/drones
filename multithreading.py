e50031dc2186e53fa1382d92c61761451408d9a2
ramosluis <luisramosmaldonado@gmail.com>
2016-11-22 13:15:45 -0700

    Update multithreading.py
    Se cambió la estructura, en vez de usar métodos ahora se usan clases para crear/usar threads

--- multithreading.py
+++ multithreading.py
@@ -7,51 +7,67 @@ import pigpio
 from threading import Thread
 from time import sleep
 
+class gpsRead(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.daemon = True
+        self.start()
+    def run(self):
+        while True:
+			gps_data = gps.readline()
+			#print gps_data
+			dataWriter.writerow([gps_data])
 
-def gps_read():
-	# guardamos valores de sensores
-	while 1:
-		gps_data = gps.readline()
-		print gps_data
 
-def lidar_read():
-	while 1:
-		byte_l = ord(lidar.read(1))
-		byte_h = ord(lidar.read(1))
-		print byte_l, byte_h
+class lidarRead(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.daemon = True
+        self.start()
+    def run(self):
+        while True:
+			byte_l = ord(lidar.read(1))
+			byte_h = ord(lidar.read(1))
+			#print byte_l, byte_h
+			dataWriter.writerow([byte_l, byte_h])
 
-def imu_read():
-	while 1:
-		# tomar valor de INT_STATUS
-		mpuIntStatus = mpu.getIntStatus()
+class imuRead(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.daemon = True
+        self.start()
+    def run(self):
+        while True:
+            # tomar valor de INT_STATUS
+			mpuIntStatus = mpu.getIntStatus()
   
-		if mpuIntStatus >= 2: # checar que este listo el interrupt de datos del DMP 
-			# tomar cuenta actual de FIFO
-			fifoCount = mpu.getFIFOCount()
-        
-			# checar si hay overflow de datos
-			if fifoCount == 1024:
-				# reinicar conteo
-				mpu.resetFIFO()
-				# y = 255
-            
-            
-			# esperar para que el paquete de datos sea del tamano correcto
-			fifoCount = mpu.getFIFOCount()
-			while fifoCount < packetSize:
+			if mpuIntStatus >= 2: # checar que este listo el interrupt de datos del DMP 
+				# tomar cuenta actual de FIFO
 				fifoCount = mpu.getFIFOCount()
         
-			result = mpu.getFIFOBytes(packetSize)
-			q = mpu.dmpGetQuaternion(result)
-			g = mpu.dmpGetGravity(q)
-			ypr = mpu.dmpGetYawPitchRoll(q, g)
+				# checar si hay overflow de datos
+				if fifoCount == 1024:
+					# reinicar conteo
+					mpu.resetFIFO()
+					# y = 255
+            
+            
+				# esperar para que el paquete de datos sea del tamano correcto
+				fifoCount = mpu.getFIFOCount()
+				while fifoCount < packetSize:
+					fifoCount = mpu.getFIFOCount()
         
-			y = ypr['yaw']
-			print ypr['yaw']
-    
-			# ver cuenta del FIFO aqui, en caso de tener mas de 1
-			# esto nos permite leer mas sin esperar un interrupt        
-			fifoCount -= packetSize 
+				result = mpu.getFIFOBytes(packetSize)
+				q = mpu.dmpGetQuaternion(result)
+				g = mpu.dmpGetGravity(q)
+				ypr = mpu.dmpGetYawPitchRoll(q, g)
+        
+				y = ypr['yaw']
+				#print y
+				dataWriter.writerow([y])
+				# ver cuenta del FIFO aqui, en caso de tener mas de 1
+				# esto nos permite leer mas sin esperar un interrupt        
+				fifoCount -= packetSize 	
 
 
 if __name__ == '__main__':
@@ -86,18 +102,13 @@ if __name__ == '__main__':
 	timestr = time.strftime("%Y_%m_%d-%H_%M_%S")
 
 	#creamos csv
-	dataFile = open('/home/pi/Documents/python/datalog/'+timestr+'.csv', 'w')
+	dataFile = open('/home/pi/datalog/'+timestr+'.csv', 'w')
 	dataWriter = csv.writer(dataFile)
 	data = []
 
-	t1 = Thread(target = gps_read)
-    	t2 = Thread(target = lidar_read)
-    	t3 = Thread(target = imu_read)
-    	t1.setDaemon(True)
-    	t2.setDaemon(True)
-    	t3.setDaemon(True)
-    	t1.start()
-    	t2.start()
-    	t3.start()
-    	while True:
-        	pass
+	# inicializar threads
+	gpsRead()
+	lidarRead()
+	imuRead()
+   	while True:
+		pass
