9ade8317f60d38c75f1544dc7514d10858785e3e
ramosluis <luisramosmaldonado@gmail.com>
2016-11-22 12:39:20 -0700

    Update multithreading.py

--- multithreading.py
+++ multithreading.py
@@ -6,7 +6,6 @@ import csv
 import pigpio
 from threading import Thread
 from time import sleep
-from os import getpid
 
 
 def gps_read():
@@ -19,16 +18,51 @@ def lidar_read():
 	while 1:
 		byte_l = ord(lidar.read(1))
 		byte_h = ord(lidar.read(1))
-	
-	print byte_l, byte_h
+		print byte_l, byte_h
 
-def imprimir():
+def imu_read():
 	while 1:
-		test_string = 'test'
-		print test_string
+		# tomar valor de INT_STATUS
+		mpuIntStatus = mpu.getIntStatus()
+  
+		if mpuIntStatus >= 2: # checar que este listo el interrupt de datos del DMP 
+			# tomar cuenta actual de FIFO
+			fifoCount = mpu.getFIFOCount()
+        
+			# checar si hay overflow de datos
+			if fifoCount == 1024:
+				# reinicar conteo
+				mpu.resetFIFO()
+				# y = 255
+            
+            
+			# esperar para que el paquete de datos sea del tamano correcto
+			fifoCount = mpu.getFIFOCount()
+			while fifoCount < packetSize:
+				fifoCount = mpu.getFIFOCount()
+        
+			result = mpu.getFIFOBytes(packetSize)
+			q = mpu.dmpGetQuaternion(result)
+			g = mpu.dmpGetGravity(q)
+			ypr = mpu.dmpGetYawPitchRoll(q, g)
+        
+			y = ypr['yaw']
+			print ypr['yaw']
+    
+			# ver cuenta del FIFO aqui, en caso de tener mas de 1
+			# esto nos permite leer mas sin esperar un interrupt        
+			fifoCount -= packetSize 
 
 
 if __name__ == '__main__':
+	# inicializacion de IMU
+	mpu = mpu6050.MPU6050()
+	mpu.dmpInitialize()
+	mpu.setDMPEnabled(True)
+
+	# get expected DMP packet size for later comparison
+	packetSize = mpu.dmpGetFIFOPacketSize() 
+
 	gps = serial.Serial(port='/dev/gps',
 					baudrate=57600,
 					parity=serial.PARITY_NONE,
@@ -58,7 +92,7 @@ if __name__ == '__main__':
 
 	t1 = Thread(target = gps_read)
     	t2 = Thread(target = lidar_read)
-    	t3 = Thread(target = imprimir)
+    	t3 = Thread(target = imu_read)
     	t1.setDaemon(True)
     	t2.setDaemon(True)
     	t3.setDaemon(True)
