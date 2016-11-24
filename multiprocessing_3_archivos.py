8633f349274a19798608251496b4834358b8751d
ramosluis <luisramosmaldonado@gmail.com>
2016-11-24 12:51:26 -0700

    Update multiprocessing_3_archivos.py

--- multiprocessing_3_archivos.py
+++ multiprocessing_3_archivos.py
@@ -1,80 +1,28 @@
 from multiprocessing import Process
 from time import sleep
+from datetime import datetime
 import serial
 import time
 import math
-import mpu6050
 import csv
 import pigpio
 
 def gpsRead():
-	#creamos csv
-	gpsFile = open('/home/pi/datalog/'+timestr+'_GPS'+'.csv', 'w')
-	gpsWriter = csv.writer(gpsFile)
-	while 1:
-		gpsWriter.writerow([gps.readline()])
-
-def lidarRead():
-	lidarFile = open('/home/pi/datalog/'+timestr+'_LIDAR'+'.csv', 'w')
-	lidarWriter = csv.writer(lidarFile)
-	while 1:
-		#byte_l = ord(lidar.read(1))
-		#byte_h = ord(lidar.read(1))
-		lidarWriter.writerow([ord(lidar.read(1)), ord(lidar.read(1))])
-
-def imuRead():
-	imuFile = open('/home/pi/datalog/'+timestr+'_IMU'+'.csv', 'w')
-	imuWriter = csv.writer(imuFile)
-	while 1:
-		# tomar valor de INT_STATUS
-		mpuIntStatus = mpu.getIntStatus()
-
-		if mpuIntStatus >= 2: # checar que este listo el interrupt de datos del DMP 
-			# tomar cuenta actual de FIFO
-			fifoCount = mpu.getFIFOCount()
-		
-			# checar si hay overflow de datos
-			if fifoCount == 1024:
-				# reinicar conteo
-				mpu.resetFIFO()
-				# y = 255
-			
-		 
-			# esperar para que el paquete de datos sea del tamano correcto
-			fifoCount = mpu.getFIFOCount()
-			while fifoCount < packetSize:
-				fifoCount = mpu.getFIFOCount()
-		
-			result = mpu.getFIFOBytes(packetSize)
-			q = mpu.dmpGetQuaternion(result)
-			g = mpu.dmpGetGravity(q)
-			ypr = mpu.dmpGetYawPitchRoll(q, g)
-			#y = ypr['yaw']
-			imuWriter.writerow([ypr['yaw']])
-			
-			# ver cuenta del FIFO aqui, en caso de tener mas de 1
-			# esto nos permite leer mas sin esperar un interrupt        
-			fifoCount -= packetSize
-
-if __name__ == '__main__':
-	# inicializacion de IMU
-	mpu = mpu6050.MPU6050()
-	mpu.dmpInitialize()
-	mpu.setDMPEnabled(True)
-
-	# get expected DMP packet size for later comparison
-	packetSize = mpu.dmpGetFIFOPacketSize() 
-
-	#variable para tomar la fecha y hora del tiempo del logger
-	timestr = time.strftime("%Y_%m_%d-%H_%M_%S")
-
 	gps = serial.Serial(port='/dev/gps',
 					baudrate=57600,
 					parity=serial.PARITY_NONE,
 					stopbits=serial.STOPBITS_ONE,
 					bytesize=serial.EIGHTBITS,
 					timeout = 1)
+	#creamos csv
+	gpsFile = open('/home/pi/datalog/'+timestamp+'_GPS'+'.csv', 'w')
+	gpsWriter = csv.writer(gpsFile)
+	while 1:
+		#timestr = time.strftime("%H_%M_%S")
+		#dt = datetime.now()
+		gpsWriter.writerow([datetime.utcnow(), gps.readline()])
 
+def lidarRead():
 	lidar = serial.Serial('/dev/ttyAMA0', 
 						baudrate = 115200,
 						bytesize = serial.EIGHTBITS,
@@ -84,7 +32,40 @@ if __name__ == '__main__':
 	# because f logic
 	lidar.close()
 	lidar.open()
-	
+
+	lidarFile = open('/home/pi/datalog/'+timestamp+'_LIDAR'+'.csv', 'w')
+	lidarWriter = csv.writer(lidarFile)
+	while 1:
+		#byte_l = ord(lidar.read(1))
+		#byte_h = ord(lidar.read(1))
+		#timestr = time.strftime("%H_%M_%S")
+		#dt = datetime.now()
+		lidarWriter.writerow([datetime.utcnow(), ord(lidar.read(1)), ord(lidar.read(1))])
+
+def imuRead():
+	imu = serial.Serial('/dev/arduinoUNO', 115200, timeout = 1)
+	imuFile = open('/home/pi/datalog/'+timestamp+'_IMU'+'.csv', 'w')
+	imuWriter = csv.writer(imuFile)
+	while 1:
+			#timestr = time.strftime("%H_%M_%S")
+			#dt = datetime.now()
+			imuWriter.writerow([datetime.utcnow(), imu.readline()])
+
+def escMotor():
+	# objeto para manipular GPIO del Pi
+	pi = pigpio.pi()
+	# inicializar ESC y girar motor
+	pi.set_servo_pulsewidth(26,1000)
+	sleep(5)
+
+	while 1:
+		pi.set_servo_pulsewidth(26, 1370)
+
+if __name__ == '__main__':
+	#variable para tomar la fecha y hora del tiempo del logger
+	timestamp = time.strftime("%Y_%m_%d-%H_%M_%S")
+
+	Process(target=escMotor).start()
 	Process(target=gpsRead).start()
 	Process(target=lidarRead).start()
 	Process(target=imuRead).start()
